738426b1a424b2da9b3c19e8f55cd20ad35fd868
waterinfo <waterinfo@DESKTOP-RH9DMC7>
2020-04-09 12:06:59 +0900

    0409 이민우 오전 최종

--- src/main/webapp/resources/js/web/mng/mng.js
+++ src/main/webapp/resources/js/web/mng/mng.js
@@ -1,86 +1,54 @@
+$(document).ready(function() {
+		$('.dtl_carousel_list').bxSlider();
+	});
 
-var time; // 슬라이드 넘어가는 시간
-var $carouselLi;
-var carouselCount; // 캐러셀 사진 갯수
-var currentIndex; // 현재 보여지는 슬라이드 인덱스 값
-var caInterval;
- 
-//사진 연결
-var imgW; // 사진 한장의 너비    
-$(document).ready(function(){
- 
-    carouselInit(580, 10000);
-});
- 
-$(window).resize(function(){
-    carousel_setImgPosition();
-});
- 
-/* 초기 설정 */
-function carouselInit( height, t ){
-    /*
-     * height : 캐러셀 높이
-     * t : 사진 전환 간격 
-    */
- 
-    time = t;
-    $("#carousel_section").height(height); // 캐너셀 높이 설정
-    $carouselLi = $("#carousel_section > ul >li");
-    carouselCount = $carouselLi.length; // 캐러셀 사진 갯수
-    currentIndex = 0; // 현재 보여지는 슬라이드 인덱스 값
-    carousel_setImgPosition();
-    carousel();
-}
- 
-function carousel_setImgPosition(){
- 
-    imgW = $carouselLi.width(); // 사진 한장의 너비    
-    // 이미지 위치 조정
-    for(var i = 0; i < carouselCount; i++)
-    {
-        if( i == currentIndex)
-        {
-            $carouselLi.eq(i).css("left", 0);
-        }
-        else
-        {
-            $carouselLi.eq(i).css("left", imgW);
-        }
-    }
-}
- 
-function carousel(){
- 
-    // 사진 넘기기
-    // 사진 하나가 넘어간 후 다시 꼬리에 붙어야함
-    // 화면에 보이는 슬라이드만 보이기
-    caInterval = setInterval(function(){
-        var left = "-" + imgW;
- 
-        //현재 슬라이드를 왼쪽으로 이동 ( 마이너스 지점 )
-        $carouselLi.eq(currentIndex).animate( { left: left }, function(){
-            // 다시 오른쪽 (제자리)로 이동
-            $carouselLi.eq(currentIndex).css("left", imgW);
- 
-            if( currentIndex == ( carouselCount - 1 ) )
-            {
-                currentIndex = 0;
-            }
-            else
-            {
-                currentIndex ++;
-            }
-        } );
- 
-        // 다음 슬라이드 화면으로
-        if( currentIndex == ( carouselCount - 1 ) )
-        {
-        // 마지막 슬라이드가 넘어갈땐 처음 슬라이드가 보이도록
-            $carouselLi.eq(0).animate( { left: 0 } );
-        }
-        else
-        {
-            $carouselLi.eq(currentIndex + 1).animate( { left: 0 } );
-        }
-    }, time);
-}
+	$(function() {
+		var mng_Slider = $('.dtl_carousel_list').bxSlider({
+			/* 슬라이드 타입 설정 */
+			mode : 'horizontal',
+			// option : 'horizontal', 'vertical', 'fade'
+
+			/* 슬라이드 타입 설정 */
+			speed : 500,
+			// option : 정수
+
+			/* 슬라이드가 자동으로 전환됩니다. */
+			auto : false,
+			// option : true / false
+
+			/* 로드시 자동 표시가 시작됩니다. false이면 "시작"컨트롤을 클릭하면 슬라이드 쇼가 시작됩니다. */
+			autoStart : true,
+			// option : true / false
+
+			/* 마우스가 슬라이더 위로 움직이면 자동쇼가 멈춥니다. */
+			autoHover : false,
+			// option : true / false
+
+			/* 슬라이드가 전환 시간 */
+			pause : 4000,
+			// option : 정수
+
+			/* true이면 "다음"/ "이전"컨트롤이 추가됩니다. */
+			controls : true,
+			// option : true / false
+
+			/* true 이면 하단 pager 버튼이 추가 됩니다. */
+			pager : true,
+			// option : true / false
+
+			/* true 인 경우 마지막 슬라이에서 "다음"을 클릭하면 첫 번째 슬라이드로 전환 */
+			infiniteLoop : true,
+			// option : true / false
+
+			/* true이면 마지막 슬라이드에서 다음버튼을 숨긴다. 첫번째 슬라이드 일 경우 동일 */
+			hideControlOnEnd : false,
+			// option : true / false
+
+			/* 슬라이더 위로 마우스를 가져가면 슬러이더 일시 중지(css 전환을 사용하는 경우 기능이 작동하지 않음.) */
+			tickerHover : false,
+			// option : true / false
+
+			slideWidth : 1000
+			
+		});
+	})
